b2bd9a516f538e2361d96039787361f4fa0b1baa
montse <rodrmontserrat4@gmail.com>
2024-11-07 20:31:50 -0500

    eliminé los comentarios

--- tables.sql
+++ tables.sql
@@ -1,12 +1,11 @@
--- Aquí van los create table statements
 
 CREATE TABLE usuarios (
-    id_usuario SERIAL PRIMARY KEY,    -- ID unico para cada usuario
-    nombre VARCHAR(30) NOT NULL,     -- Nombre del usuario
-    apellido VARCHAR(30) NOT NULL,   -- Apellido del usuario
-    correo VARCHAR(100) UNIQUE NOT NULL, -- Correo electronico unico
-    password VARCHAR (255) NOT NULL,  -- Contrasenia
-    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP  -- Fecha de registro
+    id_usuario SERIAL PRIMARY KEY,   
+    nombre VARCHAR(30) NOT NULL,     
+    apellido VARCHAR(30) NOT NULL,   
+    correo VARCHAR(100) UNIQUE NOT NULL, 
+    password VARCHAR (255) NOT NULL,  
+    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP  
 );
 
 CREATE TABLE tareas (
