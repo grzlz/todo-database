44f2f70735a304f63c40291bedc0bb87bff393bd
montse <rodrmontserrat4@gmail.com>
2024-11-07 20:17:36 -0500

    create table

--- tables.sql
+++ tables.sql
@@ -1,1 +1,18 @@
 -- Aquí van los create table statements
+
+CREATE TABLE usuarios (
+    id_usuario SERIAL PRIMARY KEY,    -- ID unico para cada usuario
+    nombre VARCHAR(30) NOT NULL,     -- Nombre del usuario
+    apellido VARCHAR(30) NOT NULL,   -- Apellido del usuario
+    correo VARCHAR(100) UNIQUE NOT NULL, -- Correo electronico unico
+    password VARCHAR (255) NOT NULL,  -- Contrasenia
+    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP  -- Fecha de registro
+);
+
+CREATE TABLE tareas (
+    id_tarea SERIAL PRIMARY KEY, 
+    id_usuario INT REFERENCES usuarios(id_usuario) ON DELETE CASCADE,          
+    titulo VARCHAR(100),
+    completado BOOLEAN NOT NULL,
+    fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP 
+);
